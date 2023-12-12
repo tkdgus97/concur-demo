29e1f0cb202f01a8d7f6710c466eca628b5512ac
tkdgus97 <breakking@naver.com>
2023-12-12 12:29:15 +0900

    init commit

---
+++ src/test/java/com/tkdgus/concurrencydemo/ConcurrencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkdgus.concurrencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
